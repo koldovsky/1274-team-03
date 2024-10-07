afc334e8d93d989d16570e6441159e4935ed56a1
Mykola-Rad <radmykola1789@gmail.com>
2024-10-07 10:10:26 +0000

    top-recommendations buttons functionality added

--- js/index.product-list.js
+++ js/index.product-list.js
@@ -4,8 +4,15 @@ import { ProductsService } from "./index.products-service.js";
 export class ProductList {
   constructor() {
     this.productsService = new ProductsService();
-    this.addEventListeners();
+    this.listenForProductsRender();
   }
+
+  listenForProductsRender() {
+    document.addEventListener("productsRendered", () => {
+      this.addEventListeners();
+    });
+  }
+
   addEventListeners() {
     document.querySelectorAll(".product-card__add-to-cart").forEach((btn) => {
       btn.addEventListener("click", this.addProductToCart.bind(this));
